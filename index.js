c1aaced0fb2a50c427114e59af400f3eb5329f8c
Juan Toro <jct20xx@gmail.com>
2022-05-07 00:34:42 -0400

    commit tree a7c2e042fdc3a8a351b7d1ddbfbd6ad69153cc54

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(userName, userEmail){
+        return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body:JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(response => response.json())
+      .then((user) => {
+        const Id = user.id
+        const p = document.createElement('p');
+        p.innerHTML = Id
+        document.body.appendChild(p);
+    })
+    .catch(error => {
+        const message = error
+        const p = document.createElement('p')
+        p.innerHTML = 'Unauthorized Access'
+        document.body.appendChild(p)
+    })
+}
